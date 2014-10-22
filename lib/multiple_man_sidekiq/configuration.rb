12507b93e1301141ff3b11ec3bfe05194aa7c1ea
Ryan Brunner <ryan@influitive.com>
2014-10-21 20:46:54 -0400

    Configuration file missing

---
+++ lib/multiple_man_sidekiq/configuration.rb
@@ -0,0 +1,21 @@
+module MultipleMan
+  class Configuration
+
+    def initialize
+      self.sidekiq_queue = {}
+    end
+
+    attr_reader :sidekiq_queue
+
+  private
+    attr_writer :sidekiq_queue
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.configure
+    yield(configuration) if block_given?
+  end
+end
